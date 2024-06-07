83119ccdd25da986a5e279f87bd5f53588c04d29
Krsmanovic Djordje (uif72067) <djordje1499@gmail.com>
2024-06-07 10:36:24 +0200

    feat: remove authorization from services and add it in api gateway

--- Booking.Microservices/Gateway.Api/Program.cs
+++ Booking.Microservices/Gateway.Api/Program.cs
@@ -1,16 +1,62 @@
+using Booking.BuildingBlocks.Domain.Enums;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+
+
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "Booking",
+            ValidAudience = "Booking",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my_awesome_secret_key_the_best_in_world"))
+        };
+    });
+
+
+
+builder.Services.AddAuthorization(options =>
+{
+    AddPolicy(options, "ChangeUserInfo", "Host", Permission.ChangeUserInfo.ToString());
+    AddPolicy(options, "HostAccommodationOperations", "Host", Permission.HostAccommodationOperations.ToString());
+    AddPolicy(options, "HostReservationOperations", "Host", Permission.HostReservationOperations.ToString());
+    AddPolicy(options, "GuestReservationOperations", "Guest", Permission.GuestReservationOperations.ToString());
+    AddPolicy(options, "GetGuestReservations", "Guest", Permission.GetGuestReservations.ToString());
+    AddPolicy(options, "GetHostReservations", "Host", Permission.GetHostReservations.ToString());
+    AddPolicy(options, "SubscriptionOperations", "Host", Permission.SubscriptionOperations.ToString());
+    AddPolicy(options, "GuestReservationPayment", "Guest", Permission.GuestReservationPayment.ToString());
+    AddPolicy(options, "HostReservationPayment", "Host", Permission.HostReservationPayment.ToString());
+    AddPolicy(options, "ReservationOperations", "Host", Permission.ReservationOperations.ToString());
+    AddPolicy(options, "InvoicesOperations", "Host", Permission.InvoicesOperations.ToString());
+    AddPolicy(options, "CreateReview", "Guest", Permission.CreateReview.ToString());
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-
-builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 
 
@@ -25,10 +71,21 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
 app.UseAuthentication();
+app.UseAuthorization();
+
 
 app.MapReverseProxy();
 
 app.Run();
+
+void AddPolicy(AuthorizationOptions options, string policyName, string role, string permission)
+{
+    options.AddPolicy(
+        policyName,
+        policy => policy
+            .RequireAuthenticatedUser()
+            .RequireRole(role)
+            .RequireClaim("permission", permission));
+}
