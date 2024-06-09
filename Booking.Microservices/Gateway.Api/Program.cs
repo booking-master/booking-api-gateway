968450b6a27b37786d526b2c9c6f864e08615501
Krsmanovic Djordje (uif72067) <djordje1499@gmail.com>
2024-06-09 19:43:22 +0200

    feat: add c4 and sequence diagram for microservices architecture

--- Booking.Microservices/Gateway.Api/Program.cs
+++ Booking.Microservices/Gateway.Api/Program.cs
@@ -3,15 +3,9 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-
-
-
-
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
